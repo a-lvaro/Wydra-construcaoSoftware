0a743fb5697373275cffe78414589df5d6ebf682
greff <vitorgreffdias@gmail.com>
2023-03-08 19:02:43 -0300

    setup do projeto vue

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Login from './components/Login.vue'
+</script>
+
+
+<template>
+  <div>
+    <header class="topo">
+      <img class = "lontra" src="../wydra.png" alt="Lontra">
+    </header>
+      <body > 
+          <Login/>
+          <!-- <RouterLink to="/login">Login</RouterLink> -->
+          <RouterView />
+      </body>
+
+  </div>
+
+
+</template> 
+
+<style scoped>
+  
+
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../components/Login.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
+  ]
+})
+
+export default router
