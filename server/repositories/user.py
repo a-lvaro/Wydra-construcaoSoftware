d46cfe060c72ed2cd525f96a5cedb4a10f31ddfa
anabravn <bravinheloisa@gmail.com>
2023-03-10 20:31:11 -0300

    fix user password error

--- server/repositories/user.py
+++ server/repositories/user.py
@@ -1,26 +1,26 @@
 import hashlib
 from sqlalchemy.orm import Session
 
-from . import db
+from .db import *
 
 class userRepository:
     @staticmethod
     def get(db : Session, id : int):
-        return db.query(db.models.User).filter(db.models.User.id == id).first()
+        return db.query(models.User).filter(models.User.id == id).first()
 
     @staticmethod
     def get_by_email(db : Session, email : str):
-        return db.query(db.models.User).filter(db.models.User.email == email).first()
+        return db.query(models.User).filter(models.User.email == email).first()
 
     @staticmethod
     def get_by_name(db : Session, name : str, skip : int = 0, limit : int = 100):
-        return db.query(db.models.User).filter(name in db.models.User.name).offset(skip).limit(limit).all()
+        return db.query(models.User).filter(name in models.User.name).offset(skip).limit(limit).all()
 
     @staticmethod
-    def create_user(db : Session, user : db.schemas.UserCreate):
+    def create_user(db : Session, user : schemas.UserCreate):
         hashed = hashlib.sha256(user.password.encode()).hexdigest()
 
-        db_user = db.models.User(name=user.name, email=user.email, password=hashed)
+        db_user = models.User(name=user.name, email=user.email, password=hashed)
         db.add(db_user)
         
         db.commit()
@@ -29,7 +29,7 @@ class userRepository:
         return db_user
 
     def delete(db : Session, id : int):
-        user = db.query(db.models.User).filter(db.models.User.id == id).first()
+        user = db.query(models.User).filter(models.User.id == id).first()
         
         if user:
             db.delete(user)
--- server/routers/user.py
+++ server/routers/user.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
-from controller import userController
+from repositories import userRepository
+from repositories.db import Session
+from repositories.db import schemas
 
 userRouter = APIRouter(
         prefix="/user"
-        )
+    )
 
-@userRouter.get("/{id}")
+@userRouter.get("/{id}", response_model=schemas.User)
 def get_user(id : int):
-    c = userController()
-    return c.get_user(id)
+    db = Session()    
+    return userRepository.get(db, id)
