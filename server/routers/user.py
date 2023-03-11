6cc113e49fa2e9a083abba6fdcafc554e10c799d
anabravn <bravinheloisa@gmail.com>
2023-03-10 23:57:18 -0300

    handle user not found error

--- server/routers/user.py
+++ server/routers/user.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
+
 from repositories import userRepository
 from repositories.db import Session
 from repositories.db import schemas
@@ -9,5 +11,10 @@ userRouter = APIRouter(
 
 @userRouter.get("/{id}", response_model=schemas.User)
 def get_user(id : int):
-    db = Session()    
-    return userRepository.get(db, id)
+    db = Session()   
+    user = userRepository.get(db, id)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não existe")
+
+    return user
