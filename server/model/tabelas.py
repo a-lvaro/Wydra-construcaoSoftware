d2ffbba3d9a91263197367f60e063bd552dc3828
a-lvaro <aaflneto@gmail.com>
2023-03-11 00:18:45 -0300

    tabelas funcionando

--- server/model/tabelas.py
+++ server/model/tabelas.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime, create_engine
+from sqlalchemy.orm import sessionmaker
 from bcrypt import hashpw, gensalt, checkpw
+from datetime import datetime
 
 
 Base = declarative_base()
@@ -15,6 +16,7 @@ class Usuario(Base):
     email = Column("EMAIL", String(30), nullable=False)
     senha = Column("SENHA", String(64), nullable=False)
     caminho_foto = Column("CAMINHO_FOTO", String(100), nullable=True)
+    data_cadastro = Column("DATA_CADASTRO", DateTime, nullable=False)
 
     def __repr__(self) -> str:
         return f"usuário(id={self.id!r}, nome={self.nome!r}, email={self.email!r})"
@@ -24,6 +26,7 @@ class Usuario(Base):
         self.email = email
         self.senha = hashpw(senha.encode('utf-8'), gensalt()).decode('utf-8')
         self.caminho_foto = caminho_foto
+        self.data_cadastro = datetime.now()
 
     @staticmethod
     def authenticate(session, email, senha):
@@ -38,7 +41,7 @@ class ObrasEstante(Base):
     __tablename__ = "ESTANTE"
 
     id_usuario = Column("ID_USUARIO", Integer, ForeignKey(
-        'usuario.id_usuario'), primary_key=True)
+        'USUARIO.ID_USUARIO'), primary_key=True)
     id_obra = Column("ID_OBRA", Integer, primary_key=True)
     estado = Column(Enum('Lista de Desejos', 'Em progresso',
                     'Finalizada', 'Abandonada', name='ESTADO'), nullable=False)
@@ -46,13 +49,24 @@ class ObrasEstante(Base):
     data_inicio = Column("DATA_INICIO", DateTime, nullable=True)
     data_fim = Column("DATA_FIM", DateTime, nullable=True)
 
-    user_id = Column(Integer, ForeignKey('usuario.id_usuario'))
-    usuario = relationship("Usuario", backref="obras_estante")
-
     def __init__(self, id_usuario, id_obra, estado, nota, data_inicio, data_fim):
-        self.id_usuario = id_usuario
+        # self.id_usuario = id_usuario
         self.id_obra = id_obra
         self.estado = estado
         self.nota = nota
         self.data_inicio = data_inicio
         self.data_fim = data_fim
+
+    def __repr__(self):
+        return f"ObrasEstante(id_usuario={self.id_usuario!r}, id_obra={self.id_obra!r}, estado={self.estado!r}, nota={self.nota!r}, data_inicio={self.data_inicio!r}, data_fim={self.data_fim!r})"
+
+
+engine = create_engine('sqlite:///server/model/database.db', echo=True)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+usuario = Usuario("João", "ola@gmail.com", "123456")
+session.add(usuario)
+session.commit()
