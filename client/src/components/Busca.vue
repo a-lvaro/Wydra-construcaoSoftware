c9b10cc31ef52c0b67508790a10d0b605c31bbf6
Rac1805 <romulo.mincache@gmail.com>
2023-03-16 10:47:48 -0300

    página de busca finalizada

--- client/src/components/Busca.vue
+++ client/src/components/Busca.vue
@@ -2,10 +2,22 @@
     <div>
         <form class="retangulo-busca" @submit.prevent="fazerBusca">
             <div class="titulo">
-                <h1>Buscar</h1>
+                <h1>Busca</h1>
+            </div>
+            <div class="fitro-categoria">
+                <label class="nome-filtro">Categoria
+                    <select class="dropdown-categoria" required v-model="categoria_selecionada">
+                        <option value="">Selecione o que deseja buscar</option>
+                        <option v-for="categoria in categorias" v-bind:value="categoria.id_categoria">
+                            {{ categoria.nome }}
+                        </option>
+                    </select>
+                </label>
             </div>
             <div class="campo-busca">
-                <input type="search" id="busca" name="busca">
+                <label class="nome-campo-busca">Nome
+                    <input placeholder="Digite o nome" type="search" id="busca" name="busca" required v-model="string_busca" />
+                </label>
             </div>
             <div class="botao-buscar">
                 <Botao texto="Buscar" />
@@ -21,44 +33,78 @@ form {
     align-items: center;
 }
 
+.titulo{
+    padding: 30px;
+}
+
 .retangulo-busca {
-  margin: 50px auto;
-  background-color: white;
-  width: 420px;
-  height: 600px;
-  border-radius: 20px;
-  border: 2px solid #000000ba;
+    margin: 50px auto;
+    background-color: white;
+    width: 420px;
+    height: 410px;
+    border-radius: 20px;
+    border: 2px solid #000000ba;
+}
+
+.nome-filtro,
+.nome-campo-busca{
+    font-size:large;
+    display: flex;
+    flex-direction: column;
+}
+
+.dropdown-categoria{
+    padding: 12px;
+    align-content: center;
+    margin-bottom: 30px;
+    border: 2px solid #0c0909;
+    border-radius: 10px;
+    width: 270px;
+    box-sizing: border-box;
 }
 
 input[type="search"] {
     padding: 12px;
     align-content: center;
     margin-bottom: 30px;
-    border: 3px solid #0c0909;
+    border: 2px solid #0c0909;
     border-radius: 10px;
-    width: 100%;
-    max-width: 320px;
+    width: 270px;
     box-sizing: border-box;
 }
+
+.botao-buscar{
+    padding-bottom: 30px;
+}
 </style>
+
 <script>
 export default {
     name: "Busca",
     data() {
         return {
-            string_busca: ''
+            categorias: [
+                { id_categoria: 0, nome: "Usuário" },
+                { id_categoria: 1, nome: "Livro" },
+                { id_categoria: 2, nome: "Filme" },
+                { id_categoria: 3, nome: "Série" },
+                { id_categoria: 4, nome: "Jogo" },
+                { id_categoria: 5, nome: "Álbum" }
+            ],
+            string_busca: '',
+            categoria_selecionada: '',
         }
     },
 
     methods: {
         fazerBusca() {
             const data = {
-                tipo_busca: this.tipo_busca,
+                categoria_selecionada: this.categoria_selecionada,
                 string_busca: this.string_busca
             }
             console.log(data)
         },
-    },
+    }
 }
 </script>
 
