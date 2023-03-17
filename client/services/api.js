3958c106521eb71d3b09830dd50b010f06932e6d
greff <vitorgreffdias@gmail.com>
2023-03-17 16:21:36 -0300

    inicio da api

---
+++ client/services/api.js
@@ -0,0 +1,44 @@
+function getMovies(name) {
+    return fetch("https://api.themoviedb.org/3/search/movie?api_key=158133b16a544083e8506dccf5af2bd4&language=pt-BR&query=Shrek&page=5&include_adult=true").then((res) => res.json());
+}
+
+function getProductsById(id) {
+    return fetch(`http://localhost:3333/products/${id}`).then((res) => res.json());
+}
+
+function getProductsPetshop(id) {
+    return fetch(`http://localhost:3333/products/petshop/${id}`).then((res) => res.json());
+}
+
+function createProducts(newProduct) {
+    return fetch('http://localhost:3333/products', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newProduct),
+    })
+        .then((res) => res.json());
+}
+
+function removeProducts(id) {
+    return fetch(`http://localhost:3333/products/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+}
+
+function editProducts(products, id) {
+    return fetch(`http://localhost:3333/products/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(products)
+    })
+        .then((res) => res.json());
+}
