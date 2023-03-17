573e9d5f630b6b32dd245ef7b9d79c629b8efd52
a-lvaro <aaflneto@gmail.com>
2023-03-17 18:53:04 -0300

    nada

--- server/model/schema.py
+++ server/model/schema.py
@@ -1,14 +1,11 @@
-from pydantic import BaseModel, validator
-from datetime import datetime
-from pydantic import BaseModel
 from typing import Union, List, Optional
 
-<< << << < HEAD
+from pydantic import BaseModel
+from datetime import datetime
 
 
 class UsuarioBase(BaseModel):
     nome: str
-
     sobrenome: str
     nick: str
 
@@ -19,24 +16,6 @@ class UsuarioCreate(UsuarioBase):
     senha_confirma: str
 
 
-== == == =
-
-
-class UsuarioBase(BaseModel):
-    nome: str
-
-
-class UsuarioCreate(UsuarioBase):
-    email: str
-    senha: str
-
-    class Config:
-        orm_mode = True
-
-
->>>>>> > main
-
-
 def get_len(arg: Union[List | int]):
     if type(arg) == int:
         return arg
@@ -44,31 +23,7 @@ def get_len(arg: Union[List | int]):
         return len(arg)
 
 
-<< << << < HEAD
 class Usuario(UsuarioBase):
 
-
-== == == =
-
-ListUsuario = ForwardRef("List[Usuario]")
-
-
-class Usuario(UsuarioBase):
-    id: int
-
-    seguidores: Optional[int]
-    _get_seguidores = validator(
-        'seguidores', pre=True, allow_reuse=True)(get_len)
-
-    seguindo: Optional[int]
-    _get_seguindo = validator('seguindo', pre=True, allow_reuse=True)(get_len)
-
-
->>>>>> > main
-
-
-class Config:
-    orm_mode = True
-
-
-Usuario.update_forward_refs()
+    class Config:
+        orm_mode = True
