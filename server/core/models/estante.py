15c843aa5a3bd83ece8196d7396cff4ae43dc1ee
anabravn <bravinheloisa@gmail.com>
2023-03-21 15:15:30 -0300

    start core module and database models

---
+++ server/core/models/estante.py
@@ -0,0 +1,25 @@
+from core.database import get_session
+
+# Entidade Estante
+class Estante(Base):
+    __tablename__ = "ESTANTE"
+
+    id_usuario = Column("ID_USUARIO", Integer, ForeignKey(
+        'USUARIO.ID_USUARIO'), primary_key=True)
+    id_obra = Column("ID_OBRA", Integer, primary_key=True)
+    estado = Column(Enum('Lista de Desejos', 'Em progresso',
+                    'Finalizada', 'Abandonada', name='ESTADO'), nullable=False)
+    tipo = Column(Enum('Manga', 'Anime', 'Livro', 'Filme',
+                  'Série', name='TIPO'), nullable=False)
+
+    data_inicio = Column("DATA_INICIO", DateTime, nullable=True)
+    data_fim = Column("DATA_FIM", DateTime, nullable=True)
+
+    def __init__(self, id_usuario, id_obra, estado, data_inicio, data_fim):
+        self.id_usuario = id_usuario
+        self.id_obra = id_obra
+        self.estado = estado
+        self.data_inicio = data_inicio
+        self.data_fim = data_fim
+
+
